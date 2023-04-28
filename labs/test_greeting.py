90377bf7c93ca67b71d6786a55dbc5bafdcaea18
mc227 <markcostales.mark@gmail.com>
2023-04-27 22:01:43 -0400

    Congratulations. You just did test-driven development!

--- labs/test_greeting.py
+++ labs/test_greeting.py
@@ -5,6 +5,6 @@ from greeting import greet
 
 class TestGreeting(unittest.TestCase):
     def test_greet(self):
-        pass
+        self.assertEqual("Hi, John", greet("John"))
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
