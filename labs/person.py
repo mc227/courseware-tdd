e989a8a8c15bbbe183c841c245ef427d6ae2f377
mc227 <markcostales.mark@gmail.com>
2023-04-28 13:12:50 -0400

    Revert "complete first lab"
    This reverts commit dd01de140eff791c16c1b9f9745c3245c00e1e9e.

--- labs/person.py
+++ labs/person.py
@@ -1,10 +1,2 @@
 class Person:
-    def __init__(self, first,last):
-        self.first = first
-        self.last = last
-    
-    def full_name(self):
-        return f"{self.first} {self.last}"
-
-    def formal_name(self,title):
-        return f"{title} {self.first} {self.last}"
+    pass
