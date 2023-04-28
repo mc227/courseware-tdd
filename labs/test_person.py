dd01de140eff791c16c1b9f9745c3245c00e1e9e
mc227 <markcostales.mark@gmail.com>
2023-04-27 22:43:41 -0400

    complete first lab

---
+++ labs/test_person.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(1, "C:/Users/Mark Costales/Desktop/dev/courseware-tdd/labs")
+
+from person import Person
+import unittest
+
+class TestPerson(unittest.TestCase):
+    def test_main(self):
+        guy = Person("John","Doe")
+        self.assertEqual(guy.first, "John")
+        self.assertEqual(guy.last, "Doe")
+        self.assertEqual(guy.full_name(), "John Doe")
+        self.assertEqual(guy.formal_name("Mr."), "Mr. John Doe")
