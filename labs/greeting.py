09e46f0d2f40b5f17979d161cb031949244f672c
mc227 <markcostales.mark@gmail.com>
2023-11-29 09:10:56 -0500

    tried putting comments and stuff

--- labs/greeting.py
+++ labs/greeting.py
@@ -1,5 +1,10 @@
-
+"""
+insert module docstring
+"""
 def greet(name):
+    """
+    insert function docstring
+    """
     return ""
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
