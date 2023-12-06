5e39b6063b976eca814f25028b4bbfa39cb0b7c7
mc227 <markcostales.mark@gmail.com>
2023-12-06 10:51:16 -0500

    I added to the greeting.py, where Tommy Wiseau's photo comes up.

--- labs/greeting.py
+++ labs/greeting.py
@@ -1,10 +1,47 @@
 """
 greeting.py
 """
+import tkinter as tk
+from PIL import Image, ImageTk, ImageSequence
+
+def display_gif(gif_path):
+    """function or method docstring"""
+    root = tk.Tk()
+    root.title("GIF Display")
+
+    # Load the GIF using PIL
+    gif = Image.open(gif_path)
+    gif_frames = [ImageTk.PhotoImage(img) for img in ImageSequence.Iterator(gif)]
+
+    # Create a label to display the GIF
+    label = tk.Label(root)
+    label.pack()
+
+    def update_frame(frame_num):
+        # Update the label with the current frame
+        label.config(image=gif_frames[frame_num])
+        root.after(100, update_frame, (frame_num + 1) % len(gif_frames))
+
+    # Start updating frames
+    update_frame(0)
+
+    root.mainloop()
+
 def greet(name):
     """
     insert function docstring
     """
-    return "Hi, " + name
+    return "Oh Hi, " + name
+
+def main():
+    """function or method docstring"""
+    print(greet("Mark"))
+
+    # Add GIF display functionality
+    gif_path = "OhHiMark.gif"  # Replace with the path to your GIF
+    display_gif(gif_path)
+
+if __name__ == "__main__":
+    main()
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
--- labs/test_textlib.py
+++ labs/test_textlib.py
@@ -6,6 +6,21 @@ class TestBodyOfText(unittest.TestCase):
     """class docstring"""
     def test_empty_story(self):
         """function or method docstring"""
-        pass
+        body_of_text = BodyOfText("")
+        self.assertEqual(body_of_text.num_paragraphs,0)
+
+    def test_single_paragraph(self):
+        """function docstring"""
+        body_of_text = BodyOfText("single paragraph.")
+        self.assertEqual(body_of_text.num_paragraphs,1)
+
+    test_several_paragraphs(self):
+        """function docstring"""
+        body_of_text = BodyOfText("""First Paragraph.
+        
+        Second Paragraph.
+        
+        Third Paragraph.""")
+        self.assertEqual()
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
