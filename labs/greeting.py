1e32cfd948b6f3394b661207915aac44cf9d4132
mc227 <markcostales.mark@gmail.com>
2023-12-06 15:00:51 -0500

    completed lab-simple.txt

--- labs/greeting.py
+++ labs/greeting.py
@@ -6,5 +6,6 @@ def greet(name):
     insert function docstring
     """
     return "Hi, " + name
+    # pass
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
--- labs/test_person.py
+++ labs/test_person.py
@@ -15,5 +15,5 @@ class TestPerson(unittest.TestCase):
         guy = Person("John", "Doe")
         self.assertEqual("John",guy.first)
         self.assertEqual("Doe",guy.last)
-        self.assertEqual(guy.full_name(), "John Doe")
-        self.assertEqual(guy.formal_name("Mr."), "Mr. John Doe")
+        self.assertEqual("John Doe", guy.full_name())
+        self.assertEqual("Mr. John Doe", guy.formal_name("Mr."))
