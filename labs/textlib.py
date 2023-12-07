12b274a7f565bd6a276ab799726857d89c22b656
mc227 <markcostales.mark@gmail.com>
2023-12-07 13:59:50 -0500

    completed lab-unittest including extra credit

--- labs/textlib.py
+++ labs/textlib.py
@@ -5,29 +5,17 @@ class BodyOfText:
         self.text = text
 
     def num_paragraphs(self):
-        """Return the number of paragraphs"""
-        return max(1, len(self.text.split("\n\n")))
+        """function or method docstring"""
+        if self.text == "":
+            return 0
+        # elif self.text.count("\n\n") == 0:
+        #     return 1
+        else:
+            return self.text.count("\n\n") + 1
 
     def paragraphs(self):
-        """Return a list of paragraphs"""
-        return [paragraph.strip() for paragraph in self.text.split("\n\n") if paragraph.strip()]
+        """Stub method to return an empty list"""
+        return [paragraph.strip() for paragraph in self.text.split("\n\n")]
 
-# Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
-
-# if __name__ == "__main__":
-#     bodyOfText = BodyOfText("""Lorem ipsum dolor sit amet, consectetur \
-# adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore \
-# magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse \
-# in est. Eu lobortis elementum nibh tellus molestie. Urna neque viverra \
-# justo nec ultrices. Cum sociis natoque penatibus et magnis dis. Maecenas \
-# sed enim ut sem viverra aliquet. Pulvinar pellentesque habitant morbi \
-# tristique senectus et. Posuere ac ut consequat semper viverra nam. \
-# Eros in cursus turpis massa tincidunt dui ut. Egestas pretium \
-# aenean pharetra magna ac placerat. Proin sed libero enim sed. \
-# Gravida dictum fusce ut placerat orci nulla pellentesque \
-# dignissim enim. Dapibus ultrices in iaculis nunc sed augue \
-# lacus viverra vitae. Et odio pellentesque diam volutpat \
-# commodo sed. Nisl pretium fusce id velit ut tortor \
-# pretium viverra suspendisse.""")
-
-#     print(bodyOfText.num_paragraphs())
+if __name__ == "__main__":
+    bodyOfText = BodyOfText("""""")
