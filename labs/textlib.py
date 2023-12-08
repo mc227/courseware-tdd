131ac4acda8a313131eb1605c593479868ea226b
mc227 <markcostales.mark@gmail.com>
2023-12-08 11:07:47 -0500

    removed unnecessary elif statement

--- labs/textlib.py
+++ labs/textlib.py
@@ -8,11 +8,19 @@ class BodyOfText:
         """function or method docstring"""
         if self.text == "":
             return 0
-        # elif self.text.count("\n\n") == 0:
-        #     return 1
         else:
             return self.text.count("\n\n") + 1
 
     def paragraphs(self):
-        """Stub method to return an empty list"""
+        """Stub method to return a list strings.
+        These strings represent a paragraph"""
         return [paragraph.strip() for paragraph in self.text.split("\n\n")]
+
+    def wordcounts(self):
+        """
+        `wordcounts` counts how many times each word occurs. 
+        
+        The method returns a dictionary mapping the lower-cased word to the number of
+        occurrences. 
+        Case and punctuation are ignored
+        """
