08fd6764f1e44706e9f0cd4399cf776e180e9d04
mc227 <markcostales.mark@gmail.com>
2023-12-08 11:44:59 -0500

    added items for test_calculate method

--- labs/pricing.py
+++ labs/pricing.py
@@ -1,5 +1,4 @@
 """pricing.py"""
 def calculate(price, tax=0, discount=0):
     """calculate method"""
-    return round((price - discount) * (1+tax), 2)
-    
+    return round((price - discount) * (1 + tax), 2)
---
+++ labs/test_pricing.py
@@ -0,0 +1,15 @@
+"""test_pricing.py"""
+import unittest
+
+from pricing import calculate
+
+class TestPricing(unittest.TestCase):
+    """TestPricing class"""
+    def test_calculate(self):
+        """test_calculate method"""
+        items = (
+            {'case': 'No tax, no discount', 'price': 10, 'tax': 0, 'discount': 0, 'net_price': 10},
+            {'case': 'Has tax, no discount', 'price': 10, 'tax': 0.1, 'discount': 0, 'net_price': 10},
+            {'case': 'No tax, has discount', 'price': 10, 'tax': 0, 'discount': 1, 'net_price': 10},
+            {'case': 'Has tax, has discount', 'price': 10, 'tax': 0.1, 'discount': 1, 'net_price': 9.9},
+        )
