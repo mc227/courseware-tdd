77f8f3a773c5e3b2699c4ab051cbb3a331b01d13
mc227 <markcostales.mark@gmail.com>
2023-12-08 11:45:59 -0500

    made pylint happy

--- labs/test_pricing.py
+++ labs/test_pricing.py
@@ -9,7 +9,10 @@ class TestPricing(unittest.TestCase):
         """test_calculate method"""
         items = (
             {'case': 'No tax, no discount', 'price': 10, 'tax': 0, 'discount': 0, 'net_price': 10},
-            {'case': 'Has tax, no discount', 'price': 10, 'tax': 0.1, 'discount': 0, 'net_price': 10},
-            {'case': 'No tax, has discount', 'price': 10, 'tax': 0, 'discount': 1, 'net_price': 10},
-            {'case': 'Has tax, has discount', 'price': 10, 'tax': 0.1, 'discount': 1, 'net_price': 9.9},
+            {'case': 'Has tax, no discount', 'price': 10, 'tax': 0.1, 'discount': 0,
+            'net_price': 10},
+            {'case': 'No tax, has discount', 'price': 10, 'tax': 0, 'discount': 1,
+            'net_price': 10},
+            {'case': 'Has tax, has discount', 'price': 10, 'tax': 0.1, 'discount': 1,
+            'net_price': 9.9},
         )
